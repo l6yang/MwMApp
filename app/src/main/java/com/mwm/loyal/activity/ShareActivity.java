f33d650700601cebb2943309dcae40184ff37d73
l6yang <zhangluyang112@163.com>
2017-03-13 16:18:41 +0800

    apk下载更新监听进度

---
+++ app/src/main/java/com/mwm/loyal/activity/ShareActivity.java
@@ -0,0 +1,170 @@
+package com.mwm.loyal.activity;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.databinding.DataBindingUtil;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mwm.loyal.R;
+import com.mwm.loyal.base.BaseSwipeActivity;
+import com.mwm.loyal.databinding.ActivityShareBinding;
+import com.mwm.loyal.libs.manager.AppAdapter;
+import com.mwm.loyal.libs.manager.AppBean;
+import com.mwm.loyal.utils.PreferencesUtil;
+import com.mwm.loyal.utils.ResUtil;
+import com.mwm.loyal.utils.TransManage;
+import com.yalantis.phoenix.PullToRefreshView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ShareActivity extends BaseSwipeActivity implements View.OnClickListener, PullToRefreshView.OnRefreshListener {
+    @BindView(R.id.pub_title)
+    TextView pubTitle;
+    @BindView(R.id.pub_back)
+    ImageView pubBack;
+    @BindView(R.id.pub_menu)
+    ImageView pubMenu;
+    @BindView(R.id.recycle_list)
+    RecyclerView recyclerView;
+    @BindView(R.id.pull_to_refresh)
+    PullToRefreshView pullToRefreshView;
+    private QueryAppAsync mQueryAuth;
+    private List<AppBean> appList = new ArrayList<>();
+    private AppAdapter appAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityShareBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_share);
+        binding.setDrawable(ResUtil.getBackground(this));
+        ButterKnife.bind(this);
+        TransManage.setTranslucentStatus(this);
+        initViews();
+        appAdapter = new AppAdapter(this, appList);
+        recyclerView.setAdapter(appAdapter);
+        queryList();
+    }
+
+    private void queryList() {
+        if (mQueryAuth != null)
+            return;
+        mQueryAuth = new QueryAppAsync();
+        mQueryAuth.execute();
+    }
+
+    private void initViews() {
+        pubMenu.setVisibility(View.GONE);
+        pubTitle.setText("分享");
+        pubBack.setOnClickListener(this);
+        pullToRefreshView.setEnabled(false);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        pullToRefreshView.setOnRefreshListener(this);
+    }
+
+    @Override
+    public int setEdgePosition() {
+        return LEFT;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.pub_back:
+                finish();
+                break;
+        }
+    }
+
+    @Override
+    public void onRefresh() {
+        appAdapter.clear();
+        queryList();
+        pullToRefreshView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                pullToRefreshView.setRefreshing(false);
+            }
+        }, 2000);
+    }
+
+    private class QueryAppAsync extends AsyncTask<Void, Integer, Void> {
+        private int totalApps;
+        private int actualApps;
+
+        QueryAppAsync() {
+            progressDialog.show();
+            actualApps = 0;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            final PackageManager packageManager = getPackageManager();
+            List<PackageInfo> packages = packageManager.getInstalledPackages(PackageManager.GET_META_DATA);
+            Set<String> hiddenApps = PreferencesUtil.getHiddenApps(ShareActivity.this);
+            totalApps = packages.size() + hiddenApps.size();
+            Collections.sort(packages, new Comparator<PackageInfo>() {
+                @Override
+                public int compare(PackageInfo p1, PackageInfo p2) {
+                    return packageManager.getApplicationLabel(p1.applicationInfo).toString().toLowerCase().compareTo(packageManager.getApplicationLabel(p2.applicationInfo).toString().toLowerCase());
+                }
+            });
+
+            for (PackageInfo packageInfo : packages) {
+                if (!(packageManager.getApplicationLabel(packageInfo.applicationInfo).equals("") || packageInfo.packageName.equals(""))) {
+                    if ((packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
+                        try {
+                            AppBean tempApp = new AppBean(packageManager.getApplicationLabel(packageInfo.applicationInfo).toString(), packageInfo.packageName, packageInfo.versionName, packageInfo.applicationInfo.sourceDir, packageInfo.applicationInfo.dataDir, packageManager.getApplicationIcon(packageInfo.applicationInfo), false);
+                            appList.add(tempApp);
+                        } catch (OutOfMemoryError e) {
+                            AppBean tempApp = new AppBean(packageManager.getApplicationLabel(packageInfo.applicationInfo).toString(), packageInfo.packageName, packageInfo.versionName, packageInfo.applicationInfo.sourceDir, packageInfo.applicationInfo.dataDir, getDrawable(R.mipmap.ic_launcher_round), false);
+                            appList.add(tempApp);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                actualApps++;
+                publishProgress((actualApps * 100) / totalApps);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressDialog.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progressDialog.dismiss();
+            mQueryAuth = null;
+            appAdapter.refreshData(appList);
+            pullToRefreshView.setEnabled(true);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            mQueryAuth = null;
+        }
+    }
+}
